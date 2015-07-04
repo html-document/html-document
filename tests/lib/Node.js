c59f5ab6d2bfe0d0f08bf1a73343f5e9d35ab4e2
Christophe Hurpeau <christophe@hurpeau.com>
2015-07-04 23:41:21 +0200

    0.5.1 add node attributes test and update Authors

---
+++ tests/lib/Node.js
@@ -0,0 +1,19 @@
+/* global test */
+'use strict';
+
+var assert = require('proclaim');
+var lib = '../../lib/';
+
+var Document = require(lib + 'Document').Document;
+
+test('Node attributes', function () {
+    var document = new Document();
+    var div = document.createElement('div');
+    assert.isUndefined(div.getAttribute('id'));
+    assert.isFalse(div.hasAttribute('id'));
+    div.setAttribute('id', 'testid');
+    assert.strictEqual(div.getAttribute('id'), 'testid');
+    assert.isTrue(div.hasAttribute('id'));
+    assert.strictEqual(div.outerHTML, '<div id="testid"></div>');
+});
+//# sourceMappingURL=Node.js.map
---
+++ tests/src/Node.js
@@ -0,0 +1,16 @@
+/* global test */
+var assert = require('proclaim');
+var lib = '../../lib/';
+
+var Document = require(lib + 'Document').Document;
+
+test('Node attributes', () => {
+    var document = new Document();
+    var div = document.createElement('div');
+    assert.isUndefined(div.getAttribute('id'));
+    assert.isFalse(div.hasAttribute('id'));
+    div.setAttribute('id', 'testid');
+    assert.strictEqual(div.getAttribute('id'), 'testid');
+    assert.isTrue(div.hasAttribute('id'));
+    assert.strictEqual(div.outerHTML, '<div id="testid"></div>');
+});
