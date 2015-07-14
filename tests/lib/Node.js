e4f1b72d74a04b0c1699e3ba71b822acac7daf00
Christophe Hurpeau <christophe@hurpeau.com>
2015-07-14 15:35:06 +0200

    0.8.0

--- tests/lib/Node.js
+++ tests/lib/Node.js
@@ -10,6 +10,7 @@ var _proclaim2 = _interopRequireDefault(_proclaim);
 var lib = '../../lib/';
 
 var Document = require(lib + 'Document');
+var Event = require(lib + 'Event');
 
 test('Node attributes', function () {
     var document = new Document();
@@ -20,5 +21,57 @@ test('Node attributes', function () {
     _proclaim2['default'].strictEqual(div.getAttribute('id'), 'testid');
     _proclaim2['default'].isTrue(div.hasAttribute('id'));
     _proclaim2['default'].strictEqual(div.outerHTML, '<div id="testid"></div>');
+});
+
+test('Node events attach', function () {
+    var document = new Document();
+    var div = document.createElement('div');
+    div.addEventListener('click', function () {}, true);
+    _proclaim2['default'].isTrue(div.hasOwnProperty('_eventsCapturingPhase'));
+    _proclaim2['default'].isTrue(div._eventsCapturingPhase.has('click'));
+    _proclaim2['default'].strictEqual(div._eventsCapturingPhase.get('click').length, 1);
+});
+
+test('Node events remove', function () {
+    var document = new Document();
+    var div = document.createElement('div');
+
+    function handler() {}
+
+    div.addEventListener('click', handler, true);
+    _proclaim2['default'].isTrue(div.hasOwnProperty('_eventsCapturingPhase'));
+    _proclaim2['default'].isTrue(div._eventsCapturingPhase.has('click'));
+    _proclaim2['default'].strictEqual(div._eventsCapturingPhase.get('click').length, 1);
+    div.removeEventListener('click', handler, true);
+    _proclaim2['default'].isTrue(div.hasOwnProperty('_eventsCapturingPhase'));
+    _proclaim2['default'].isTrue(div._eventsCapturingPhase.has('click'));
+    _proclaim2['default'].strictEqual(div._eventsCapturingPhase.get('click').length, 0);
+});
+
+test('Node events type normalize', function () {
+    var document = new Document();
+    var div = document.createElement('div');
+
+    function handler() {}
+
+    div.addEventListener('DOMLoadReady', handler, true);
+    _proclaim2['default'].isTrue(div.hasOwnProperty('_eventsCapturingPhase'));
+    _proclaim2['default'].isTrue(div._eventsCapturingPhase.has('domloadready'));
+});
+
+test('Node event dispatch', function () {
+    var document = new Document();
+    var div = document.createElement('div');
+    var event = new Event('DOMLoadReady');
+
+    function handler(e) {
+        _proclaim2['default'].equal(e.type, 'domloadready');
+        _proclaim2['default'].equal(e.target, div);
+    }
+
+    div.addEventListener('DOMLoadReady', handler, true);
+    _proclaim2['default'].isTrue(div.hasOwnProperty('_eventsCapturingPhase'));
+    _proclaim2['default'].isTrue(div._eventsCapturingPhase.has('domloadready'));
+    div.dispatchEvent(event);
 });
 //# sourceMappingURL=Node.js.map
